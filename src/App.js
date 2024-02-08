a48214bbbfc643e6e097c6329fe04a2936e69d25
Hmorgan22 <huntermorgan1219@yahoo.com>
2024-02-08 10:21:34 -0500

    created flashcard deck that can add custom cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
+      <FlashCards />
     </div>
   );
 }
 
-export default App;
+let questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
+
+//randomly shuffles the order of the questions so they are different each time
+function shuffle(arr) {
+  const newArr = [];
+  const used = [];
+
+  let i = 0;
+  while (i < arr.length) {
+    const randomNum = Math.floor(Math.random() * arr.length);
+    if (!used.includes(randomNum)) {
+      newArr.push(arr[randomNum]);
+      used.push(randomNum);
+      i++;
+    }
+  }
+  return newArr;
+}
+questions = shuffle(questions);
+
+//creates card and answer component
+function FlashCards() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  //triggers the update on the selected card
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          className={question.id === selectedId ? "selected" : ""}
+          onClick={() => handleClick(question.id)}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function SearchBar() {
+  const [q, setQ] = useState("");
+  const [a, setA] = useState("");
+  const id = Date.now();
+
+  function handleInput(e) {
+    e.preventDefault();
+    if (!q || !a) return;
+
+    const newCard = { id: id, question: q, answer: a };
+
+    setA("");
+    setQ("");
+  }
+
+  return (
+    <form onSubmit={handleInput}>
+      <input
+        type="text"
+        placeholder="Question"
+        value={q}
+        onChange={(e) => setQ(e.target.value)}
+      ></input>
+      <input
+        type="text"
+        placeholder="answer"
+        value={a}
+        onChange={(e) => setA(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
